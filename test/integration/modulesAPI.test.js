9aadc94dae08a299fe3813d3808af7f4a5f414ef
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2023-11-25 23:36:35 +0700

    updated integration testing course management api

--- test/integration/modulesAPI.test.js
+++ test/integration/modulesAPI.test.js
@@ -2,21 +2,22 @@ const request = require('supertest');
 const app = require('../../index');
 
 beforeAll(async () => {
-    const createCourseTracking = await request(app)
-                        .post('/courseTrackings')
+    const createModule = await request(app)
+                        .post('/modules')
                         .send({
-                            status : "PROGRESS",
-                            userId: "1",
-                            courseId: "5b76fce4-a584-4d47-9c70-fb38e9b5d502"
+                            title: "Dummy Modules",
+                            video: "www.video.lol",
+                            time: 45,
+                            courseId : "5b76fce4-a584-4d47-9c70-fb38e9b5d502"
                         })
-    courseTrackingDummy = createCourseTracking.body.data.id;
+    moduleDummy = createModule.body.data.id;
 })
 
-describe('moduleTrackings API', () => {
-    describe('GET /courseTrackings', () => {
-        it('should return list module tracking data', async () => {
+describe('modules API', () => {
+    describe('GET /modules', () => {
+        it('should return list modules data', async () => {
             const res = await request(app)
-                .get('/courseTrackings');
+                .get('/modules');
             expect(res.statusCode).toBe(200);
             expect(res.body).toHaveProperty('status');
             expect(res.body).toHaveProperty('message');
@@ -24,21 +25,21 @@ describe('moduleTrackings API', () => {
         
     });
 
-    describe('POST /courseTrackings/search', () => {
-        it('should return module tracking data', async () => {
+    describe('POST /modules/search', () => {
+        it('should return modules data', async () => {
             const res = await request(app)
-                .post('/courseTrackings/search')
+                .post('/modules/search')
                 .send({
-                    id: courseTrackingDummy
+                    id: moduleDummy
                 });
             expect(res.statusCode).toBe(200);
             expect(res.body).toHaveProperty('status');
             expect(res.body).toHaveProperty('message');
         }); 
 
-        it('should return module tracking data if data its not exist', async () => {
+        it('should return modules data if data its not exist', async () => {
             const res = await request(app)
-                .post('/courseTrackings/search')
+                .post('/modules/search')
                 .send({
                     id: '21edada'
                 });
@@ -48,13 +49,14 @@ describe('moduleTrackings API', () => {
         }); 
     });
 
-    describe('POST /courseTrackings', () => {
-        it('should return create module tracking data', async () => {
+    describe('POST /modules', () => {
+        it('should return create modules data', async () => {
             const res = await request(app)
-                .post('/courseTrackings')
+                .post('/modules')
                 .send({
-                    status : "PROGRESS",
-                    userId: "1",
+                    title: "Dummy Modules",
+                    video: "www.video.lol",
+                    time: 45,
                     courseId: "5b76fce4-a584-4d47-9c70-fb38e9b5d502"
                 });
             expect(res.statusCode).toBe(201);
@@ -62,33 +64,34 @@ describe('moduleTrackings API', () => {
             expect(res.body).toHaveProperty('message');
         }); 
 
-        it('should return module tracking data if body is null', async () => {
+        it('should return modules data if body is null', async () => {
             const res = await request(app)
-                .post('/courseTrackings')
+                .post('/modules')
                 .send({});
             expect(res.statusCode).toBe(400);
             expect(res.body).toHaveProperty('status');
             expect(res.body).toHaveProperty('message');
         }); 
 
-        it('should return module tracking data if invalid sort data structure', async () => {
+        it('should return modules data if invalid sort data structure', async () => {
             const res = await request(app)
-                .post('/courseTrackings')
+                .post('/modules')
                 .send({
-                    status : "PROGRESS",
-                    courseId: "5b76fce4-a584-4d47-9c70-fb38e9b5d502",
-                    userId: "1"
+                    titleA: "Dummy Modules",
+                    video: "www.video.lol",
+                    time: 45,
+                    courseId: "5b76fce4-a584-4d47-9c70-fb38e9b5d502"
                 });
             expect(res.statusCode).toBe(400);
             expect(res.body).toHaveProperty('status');
             expect(res.body).toHaveProperty('message');
         }); 
 
-        it('should return module tracking data if invalid data structure', async () => {
+        it('should return modules data if invalid data structure', async () => {
             const res = await request(app)
-                .post('/courseTrackings')
+                .post('/modules')
                 .send({
-                    title: "Dummy Module tracking",
+                    title: "Dummy Modules",
                 });
             expect(res.statusCode).toBe(400);
             expect(res.body).toHaveProperty('status');
@@ -97,37 +100,37 @@ describe('moduleTrackings API', () => {
 
     });
 
-    describe('PUT /courseTrackings', () => {
-        it('should return update module tracking data', async () => {
+    describe('PUT /modules', () => {
+        it('should return update modules data', async () => {
             const res = await request(app)
-                .put('/courseTrackings')
+                .put('/modules')
                 .send({
-                    id: courseTrackingDummy,
-                    status : "DONE",
+                    id: moduleDummy,
+                    title: "Dummy Modules",
                 });
             expect(res.statusCode).toBe(201);
             expect(res.body).toHaveProperty('status');
             expect(res.body).toHaveProperty('message');
         }); 
 
-        it('should return module tracking data if data not exist', async () => {
+        it('should return modules data if data not exist', async () => {
             const res = await request(app)
-                .post('/courseTrackings')
+                .post('/modules')
                 .send({
                     id: "dasdadas",
-                    status : "DONE",
+                    title: "Dummy Modules",
                 });
             expect(res.statusCode).toBe(400);
             expect(res.body).toHaveProperty('status');
             expect(res.body).toHaveProperty('message');
         }); 
 
-        it('should return module tracking data if invalid sort data structure', async () => {
+        it('should return modules data if invalid sort data structure', async () => {
             const res = await request(app)
-                .post('/courseTrackings')
+                .post('/modules')
                 .send({
-                    id: courseTrackingDummy,
-                    statusA : "DONE",
+                    id: moduleDummy,
+                    titleA: "Dummy Modules",
                 });
             expect(res.statusCode).toBe(400);
             expect(res.body).toHaveProperty('status');
